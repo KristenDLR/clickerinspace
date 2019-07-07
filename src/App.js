c1705d263f9dc1bb50ef57b6f3dc47d30a534055
Kristen De La Rosa <delarosa.kristen@gmail.com>
2019-07-07 00:03:33 -0400

    card body

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 // import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Nav from './component/Nav/Nav'
+// import Nav from './component/Nav/Nav'
+import Card from "./component/Card/Card";
+import Wrapper from "./component/Wrapper/Wrapper";
+import cards from "./cards.json";
 import './App.css';
 
 
 function App() {
   return (
-  //   <Router>
-  //    <>
-  //     <Nav />
-  //   </>
-  //  </Router>
-  <div></div>
+    <Wrapper>
+    <h1 className="title">Cards</h1>
+    <Card
+      name={cards[0].name}
+      image={cards[0].image}
+    />
+    <Card
+      name={cards[1].name}
+      image={cards[1].image}
+    />
+    <Card
+      name={cards[2].name}
+      image={cards[2].image}
+    />
+  </Wrapper>
   );
 }
 
