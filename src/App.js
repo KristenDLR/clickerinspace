eefb81096e581836c5e6f6fdc6e10cf96ca0e0ca
Kristen De La Rosa <delarosa.kristen@gmail.com>
2019-07-07 19:29:43 -0400

    cards then changed to arrow function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { Component } from "react";
 // import { BrowserRouter as Router, Route } from 'react-router-dom'
 // import Nav from './component/Nav/Nav'
 import Card from "./component/Card/Card";
 import Wrapper from "./component/Wrapper/Wrapper";
+import Nav from "./component/Nav/Nav";
 import cards from "./cards.json";
 import './App.css';
 
 
-function App() {
+class App extends Component {
+  //Setting this.state.cards to the cards json array
+  state = {
+    cards
+  };
+
+
+  //Map over this.state.cards and render card component for each card object
+render (){
   return (
     <Wrapper>
-    <h1 className="title">Cards</h1>
-    <Card
+    {/* <h1 className="title">Clicker in Space</h1> */}
+    <Nav> Clicker in Space</Nav>
+    {this.state.card.map(cards => (
+      <Card
+      id={cards.id}
+      key={cards.id}
+      name={cards.name}
+      image={cards.image}
+      />
+    ))}
+    {/* <Card
       name={cards[0].name}
       image={cards[0].image}
     />
@@ -23,8 +41,13 @@ function App() {
       name={cards[2].name}
       image={cards[2].image}
     />
+    <Card
+      name={cards[2].name}
+      image={cards[2].image}
+    /> */}
   </Wrapper>
   );
+}
 }
 
 export default App;
--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,4 +1,12 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 //include title, message, and score
+function Nav(props) {
+  return <nav className="Nav">Clicker In Space<
+    <p>Score: {props.score}</p>
+  </nav>
+
+}
+
+export default Nav;
