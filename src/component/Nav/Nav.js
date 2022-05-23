895c4486cd51684aeaa12795c7c1fdf7f4175ea7
Kristen De La Rosa <kristendelarosa@Kristens-MacBook-Pro.local>
2022-05-23 12:00:41 -0700

    updated nav

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./styles.css";
+import { Container, Row, Col } from "react-bootstrap";
+
 
 //include title, message, and score
 function Nav(props) {
   return <nav className="Nav">
-    <h1 className="title">Clicker in Space</h1>
-    <p>Score: {props.score}</p>
+    <Container>
+     <Row className="Nav">
+        <Col className="col" m={6}>
+    
+          <h1 className="title">Clicker in Space</h1>
+        </Col>
+        <Col className="col" m={6}>
+         <p>Score: {props.score}</p>
+        </Col>
+     </Row>
+    </Container>
+    
   </nav>
 
 }
